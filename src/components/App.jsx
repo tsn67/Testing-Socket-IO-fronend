af0c53f8af91b0e890552b57d2f8c58e8aa440ad
tsn67 <tomsebastian67@gmail.com>
2024-12-25 14:49:06 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from "react";
+import {io} from 'socket.io-client';
+
+function App() {
+
+    const [onlinecount, setOnlineCount] = useState(0);
+
+
+    useEffect(() => {
+        const socket = io('http://testing-socket-io-backend-production.up.railway.app/');
+        socket.on('init', (json) => {
+            console.log(json);
+            setOnlineCount(json.count);  
+        });
+    }, []); 
+
+    return <div>
+        <h3>online: {onlinecount == 0?"loading":onlinecount}</h3>
+    </div>
+}
+
+export default App;
